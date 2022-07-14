7eb8a2a0cdc75b5d5c8c919cbacd42fd7468a19b
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-07-14 18:35:45 -0500

    se agrega footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import ProductDetail from './components/ProductDetail/ProductDetail'
 import Home from './pages/Home'
+import Footer from './components/Footer/Footer'
 
 function App () {
   return (
@@ -9,6 +10,7 @@ function App () {
         <Route path='/' element={<Home />} />
         <Route path='/:id' element={<ProductDetail />} />
       </Routes>
+      <Footer />
     </>
   )
 }
