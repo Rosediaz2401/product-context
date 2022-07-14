ea53ce13786e864a0af25268546353684d6dbee1
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-07-13 21:21:07 -0500

    estilos producto-detail.css

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,22 +1,22 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './productdetail.css'
 
 const ProductDetail = () => {
   const [producto, setProducto] = useState({})
+  const [loading, setLoading] = useState(true)
 
   const getProductos = async () => {
     const res = await axios.get('https://ecomerce-master.herokuapp.com/api/v1/item/')
     const filterProducto = res.data.filter((item) => item._id === id)[0]
-    console.log('+>>')
     console.log(filterProducto)
     setProducto(filterProducto)
-    // setProducto(res.data)
   }
 
   useEffect(() => {
     getProductos()
-    // setLoading(false)
+    setLoading(false)
   }, [])
 
   const validacionUrl = (url) => {
@@ -29,15 +29,22 @@ const ProductDetail = () => {
   const { id } = useParams()
   return (
     <>
-      <div>
-        <img className='imagen' src={validacionUrl(producto.image) ? producto.image : validacionUrl(producto.images) ? producto.images : producto.images || 'https://cf.geekdo-images.com/camo/cba429883803dadea626df689cdbf3ddc0dc1bba/68747470733a2f2f692e696d6775722e636f6d2f456161485557462e6a7067'} alt='imagen producto' />
-        <h1> {`${producto.product_name} `}</h1>
-        <h3>{`SKU: ${producto.sku} `}</h3>
-        <h3>{`Brand: ${producto.brand} `}</h3>
-        <h3>{`Category: ${producto.category} `}</h3>
-        <h4>{`Description: ${producto.description} `}</h4>
-        <h2>{`Price: $ ${producto.price} `}</h2>
-        <button>Buy now</button>
+      {
+        loading
+    }
+      <div className='product-detail-container'>
+        <div className='product-image-detail'>
+          <img className='image-detail' src={validacionUrl(producto.image) ? producto.image : validacionUrl(producto.images) ? producto.images : producto.images || 'https://cf.geekdo-images.com/camo/cba429883803dadea626df689cdbf3ddc0dc1bba/68747470733a2f2f692e696d6775722e636f6d2f456161485557462e6a7067'} alt='imagen producto' />
+        </div>
+        <div className='product-text-detail'>
+          <h1> {`${producto.product_name} `}</h1>
+          <h3>{`SKU: ${producto.sku} `}</h3>
+          <h3>{`Brand: ${producto.brand} `}</h3>
+          <h3>{`Category: ${producto.category} `}</h3>
+          <h4 className='product-description'>{`Description: ${producto.description} `}</h4>
+          <h2>{`Price: $ ${producto.price} `}</h2>
+          <button className='btn btn-dark'>Buy now</button>
+        </div>
       </div>
     </>
   )
